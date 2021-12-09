d6a94736e434df3629eadc72a238a3918d987fd6
OPPanda <0PP4nd4@gmail.com>
2021-12-09 04:48:22 -0700

    Fixed network switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './scss/dark.scss';
 import './scss/light.scss';
 
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import { useWeb3ConnectionsContext } from './library/providers/Web3ConnectionsProvider';
+
+import { CHAIN_NAMES } from './library/constants/chains';
 
 import MainLayout from './layouts/MainLayout';
 
@@ -11,16 +15,22 @@ import NFPPage from './pages/NFPPage';
 import TestPage from './pages/test';
 
 export const App = () => {
+  const { currentChainId } = useWeb3ConnectionsContext();
+
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="/hub" element={<RewardsHubPage />}>
-            <Route path=":chain" element={<RewardsHubPage />} />
-          </Route>
+          <Route
+            path="/hub"
+            element={<Navigate to={`/${CHAIN_NAMES[currentChainId]}/hub`} />}
+          />
           <Route path="/nfp" element={<NFPPage />} />
           <Route path="/test" element={<TestPage />} />
+          <Route path="/:chain" element={<RewardsHubPage />}>
+            <Route path="hub" element={<RewardsHubPage />} />
+          </Route>
         </Route>
       </Routes>
     </HashRouter>
