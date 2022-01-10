75fdb974372e8f44a14eed13c80c2792c6de8eca
pajaroherido <pajaroherido@protonmail.ch>
2022-01-09 22:30:38 -0600

    Dynamically get every chain APR and so the highest

--- src/pages/home/BannerSection.tsx
+++ src/pages/home/BannerSection.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BannerImg from '../../assets/banner-img.png';
+
+import { useConfig } from '../../library/providers/ConfigProvider';
+import usePools from '../../hooks/usePools';
+import numeral from 'numeral';
+
+const getHighestRewardAPR = (aprs: number[]) => {
+  const highestAPRs = aprs.sort((a, b) => b - a);
+  return highestAPRs?.shift() || 0;
+};
+
+const formatAPR = (apr: number): string => numeral(apr).format('0.00%');
+
 const BannerSection = () => {
+  const { readOnlyChainIds: chainIds = [] } = useConfig();
+  const allAPRs = chainIds
+    .map((chainId) => {
+      const pools = usePools(chainId);
+      return pools.map((p) => p.apr);
+    })
+    .flat(2);
+  const [highAPR, setHighAPR] = useState(0);
+  useEffect(() => {
+    const APR = getHighestRewardAPR(allAPRs);
+    setHighAPR(APR);
+  }, [highAPR, allAPRs.reduce((a, b) => a + b)]);
+  const APRBanner = highAPR ? (
+    <p>Earn up to {formatAPR(highAPR)} APR in our reward hub.</p>
+  ) : null;
   return (
     <>
       <div className={`banner-section-wrapper`}>
@@ -14,7 +41,7 @@ const BannerSection = () => {
               <p className="text-green">
                 <strong>Invest in the project today.</strong>
               </p>
-              <p>Earn up to 500% APR in our reward hub.</p>
+              {APRBanner}
             </div>
             <div className="banner-img d-block d-md-none">
               <img src={BannerImg} alt="" />
