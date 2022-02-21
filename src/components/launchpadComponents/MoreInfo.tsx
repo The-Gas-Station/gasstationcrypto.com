cc83feb10884f32bcd6bc6d45335cbab55c270d5
captnocap <admin@gasstationcrypto.com>
2022-02-20 16:03:56 -0800

    ApplyAsProject component

--- src/components/launchpadComponents/MoreInfo.tsx
+++
@@ -1,9 +0,0 @@
-const MoreInfo = () => {
-  return (
-    <div className="box2">
-      The Gas Station launchpad is the destination IDO for projects to fuel up
-      for launch
-    </div>
-  );
-};
-export default MoreInfo;
--- src/pages/LaunchPadChainPage.tsx
+++ src/pages/LaunchPadChainPage.tsx
@@ -4,9 +4,16 @@ import rocket from '../assets/rocket.svg';
 import headerlogo from '../assets/ppp.svg';
 import listing0 from '../components/launchpadComponents/listing/listing0';
 import TermsConditions from '../components/launchpadComponents/TermsConditions';
+import ApplyAsProject from '../components/launchpadComponents/ApplyAsProject';
+import LearnMore from '../components/launchpadComponents/LearnMore';
 
 export const LaunchPadChainPage = () => {
   const [showConditions, setShowConditions] = useState(true);
+  const [showApply, setShowApply] = useState(false);
+  const toggleApply = () => setShowApply(!showApply);
+  const [showLearn, setShowLearn] = useState(false);
+  const toggleLearn = () => setShowLearn(!showLearn);
+
   return (
     <>
       <TermsConditions
@@ -23,8 +30,17 @@ export const LaunchPadChainPage = () => {
       <div className="selection-row">
         <h2 className="title-2">CrowdFunding</h2>
         <h4 className="title-3">Support New Projects</h4>
-        <button className="button2">+ Apply as a Project</button>
+        <div className="button-block">
+          <button className="button2" onClick={() => toggleLearn()}>
+            Learn More
+          </button>
+          <button className="button2" onClick={() => toggleApply()}>
+            + Apply as a Project
+          </button>
+        </div>
       </div>
+      {showLearn ? <LearnMore /> : null}
+      {showApply ? <ApplyAsProject /> : null}
       <div className="flex-row d-flex flex-wrap justify-content-center">
         <PppCard stakingProps={listing0} titleProps={listing0} />
       </div>
